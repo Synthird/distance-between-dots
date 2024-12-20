e39384f1178c9d40a4759bd3344ee50671a5595f
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-19 22:17:18 -0800

    Replace spaces with tabs when code is indented

--- src/main.py
+++ src/main.py
@@ -2,27 +2,27 @@ start_dot: str = "."
 
 
 def input_number(input_message: str) -> int:
-    try:
-        number: int = int(input(f"{input_message}: "))
+	try:
+		number: int = int(input(f"{input_message}: "))
 
-        if number > -1:
-            return number
-        else:
-            raise ValueError
-    except:
-        print("!!! Cannot print dots! !!!")
-        print("Probably because you entered:")
-        print("a) Decimals")
-        print("b) Letters")
-        print("c) Symbols")
-        print("d) Spaces between numbers")
-        print("e) A negative number")
-        print("f) You exited the program")
-        raise SystemExit
+		if number > -1:
+			return number
+		else:
+			raise ValueError
+	except:
+		print("!!! Cannot print dots! !!!")
+		print("Probably because you entered:")
+		print("a) Decimals")
+		print("b) Letters")
+		print("c) Symbols")
+		print("d) Spaces between numbers")
+		print("e) A negative number")
+		print("f) You exited the program")
+		raise SystemExit
 
 
 def ask_question(question: str) -> None:
-    print(f"--- {question} ---")
+	print(f"--- {question} ---")
 
 
 ask_question("How many characters are between the two dots?")
@@ -34,10 +34,10 @@ print("Any other number = No")
 show_line: int = input_number("Show a line")
 
 if show_line == 1:
-    for _ in range(characters):
-        start_dot = f"{start_dot}_"
+	for _ in range(characters):
+		start_dot = f"{start_dot}_"
 else:
-    for _ in range(characters):
-        start_dot = f"{start_dot} "
+	for _ in range(characters):
+		start_dot = f"{start_dot} "
 
 print(f"{start_dot}.")
