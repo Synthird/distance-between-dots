ddfa8cb70f5aafc871429181ccc8a62c33538726
Synthird <105394020+Synthird@users.noreply.github.com>
2025-01-05 22:11:13 -0800

    Shortened the input_number function and removed the exit with reasons function

--- src/main.py
+++ src/main.py
@@ -1,29 +1,18 @@
 start_dot: str = "."
 
 
-def exit_with_possible_reasons() -> None:
-	print("!!! Cannot print dots! !!!")
-	print("Probably because you entered:")
-	print("a) Decimals")
-	print("b) Letters")
-	print("c) Symbols")
-	print("d) A negative number")
-	print("e) You exited the program")
-	raise SystemExit
-
-
 def input_number(input_message: str) -> int:
-	number: int = 0
-
 	try:
-		number = int(input(f"{input_message}: ").replace(" ", ""))
+		return int(input(f"{input_message}: ").replace(" ", ""))
 	except:
-		exit_with_possible_reasons()
-
-	if number > -1:
-		return number
-	else:
-		exit_with_possible_reasons()
+		print("!!! Cannot print dots! !!!")
+		print("Probably because you entered:")
+		print("a) Decimals")
+		print("b) Letters")
+		print("c) Symbols")
+		print("d) A negative number")
+		print("e) You exited the program")
+		raise SystemExit
 
 
 def ask_question(question: str) -> None:
